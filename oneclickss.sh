becefc4dfc1a2a6ac2de000550c26a3fddeedfc3
yoo <yoo@yoo.hk>
2013-08-27 13:59:43 +0800

    Update oneclickss.sh

--- oneclickss.sh
+++ oneclickss.sh
@@ -49,7 +49,7 @@ CONFIGPATH=$(pwd)/shadowsocks/config.json
 echo "Your config path is $CONFIGPATH"
 #cp ./shadowsocks/config.json $CONFIGPATH
 
-SERVERIP=$(ifconfig | grep "inet addr" | awk '{print $3}'| cut -d ':' -f 2 | grep -v 127.0.0.1 | head -n 1)
+SERVERIP=$(ifconfig | grep "inet addr" | awk '{print $2}'| cut -d ':' -f 2 | grep -v 127.0.0.1 | head -n 1)
 sed "s/127\.0\.0\.1/$SERVERIP/g" -i $CONFIGPATH
 echo "Your server ip is $SERVERIP"
 
